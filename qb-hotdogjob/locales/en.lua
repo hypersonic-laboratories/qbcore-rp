8fbd9fca3805445520300945205c285adc68fb08
Kakarot <joshuaeger@hypersoniclabs.co>
2026-07-02 11:49:05 -0500

    locale fix

--- qb-hotdogjob/locales/en.lua
+++ qb-hotdogjob/locales/en.lua
@@ -54,7 +54,8 @@ local Translations = {
     },
 }
 
-Lang = Lang or Locale:new({
+---@type { t: fun(key: string, subs?: table<string, any>): string, has: fun(key: string): boolean }
+Lang = Lang or Locale.new({
     phrases = Translations,
     warnOnMissing = true,
 })
